d5f47367ee0ad438be1ce60c70882afbb9a5bc33
Jefrey Sobreira Santos <jesobreira@yahoo.com.br>
2025-05-01 18:01:20 +0100

    add health check endpoint

---
+++ assemble/api/meta/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample, OpenApiTypes
+from ..permissions import HasValidAPIKey, TenantPermission
+from ..authentication import APIKeyAuthentication
+from datetime import datetime
+
+@extend_schema(tags=['meta'])
+class HealthCheckViewSet(viewsets.ViewSet):
+    """
+    API endpoint for health check.
+    """
+    authentication_classes = [APIKeyAuthentication]
+    permission_classes = [HasValidAPIKey, TenantPermission]
+    
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                description='Health check response',
+                examples=[
+                    OpenApiExample(
+                        'Health Check',
+                        value={
+                            "status": "ok",
+                            "timestamp": "2024-01-01T00:00:00Z"
+                        }
+                    )
+                ]
+            )
+        }
+    )
+    def list(self, request, *args, **kwargs):
+        """
+        Perform a health check.
+        """
+        return Response({
+            "status": "ok",
+            "timestamp": datetime.utcnow().isoformat() + 'Z'
+        }, status=status.HTTP_200_OK)
--- assemble/urls.py
+++ assemble/urls.py
@@ -13,6 +13,7 @@ from assemble.api.webhooks.views import WebhookViewSet
 from assemble.api.limits.views import (
     PixLimitViewSet, PixNightLimitViewSet, TedLimitViewSet, BookLimitViewSet
 )
+from assemble.api.meta.views import HealthCheckViewSet
 from rest_framework.routers import DefaultRouter
 
 # Change admin site title
@@ -32,6 +33,7 @@ router.register(r'limits/pix', PixLimitViewSet, basename='pix-limit')
 router.register(r'limits/pix-night', PixNightLimitViewSet, basename='pix-night-limit')
 router.register(r'limits/ted', TedLimitViewSet, basename='ted-limit')
 router.register(r'limits/book', BookLimitViewSet, basename='book-limit')
+router.register(r'health-check', HealthCheckViewSet, basename='health-check')
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
